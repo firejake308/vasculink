ca500d38b90b74381e904afedcf6675d568ebaef
flora-zhang-sp <flora.zhang@sailpoint.com>
2020-07-20 22:17:28 -0500

    first push.

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_redux/flutter_redux.dart';
 import 'package:redux/redux.dart';
-import 'package:vasculink/redux_demo_page.dart';
+import 'redux_demo_page.dart';
 import 'package:vasculink/state_manager.dart';
-// import 'package:vasculink/my_home_page.dart';
+//import 'package:vasculink/my_home_page.dart';
 
 void main() {
   const riskFactorNames = ["Smoker", "Obese", "Reoperation", "ESRD"];
