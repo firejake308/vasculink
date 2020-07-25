eab2345065aae4c397928c9a19f7dac0787f9ec9
flora-zhang-sp <flora.zhang@sailpoint.com>
2020-07-25 16:32:43 -0500

    add some code.

--- lib/main.dart
+++ lib/main.dart
@@ -1,54 +1,156 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_redux/flutter_redux.dart';
-import 'package:redux/redux.dart';
-import 'redux_demo_page.dart';
-import 'package:vasculink/state_manager.dart';
-//import 'package:vasculink/my_home_page.dart';
 
 void main() {
-  const riskFactorNames = ["Smoker", "Obese", "Reoperation", "ESRD"];
-  var initialState = riskFactorNames
-      .asMap()
-      .entries
-      .map((e) => RiskFactor()(e.key, e.value))
-      .toList();
-  print(initialState);
-  final store = new Store(riskFactorsReducer, initialState: initialState);
-  runApp(MyApp(store));
-}
-
-class RiskFactor() {
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  final Store<List<RiskFactor>> store;
-
-  MyApp(this.store);
-
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return new StoreProvider(
-        store: store,
-        child: MaterialApp(
-          title: 'Flutter Demo',
-          theme: ThemeData(
-            // This is the theme of your application.
-            //
-            // Try running your application with "flutter run". You'll see the
-            // application has a blue toolbar. Then, without quitting the app, try
-            // changing the primarySwatch below to Colors.green and then invoke
-            // "hot reload" (press "r" in the console where you ran "flutter run",
-            // or simply save your changes to "hot reload" in a Flutter IDE).
-            // Notice that the counter didn't reset back to zero; the application
-            // is not restarted.
-            primarySwatch: Colors.blue,
-            // This makes the visual density adapt to the platform that you run
-            // the app on. For desktop platforms, the controls will be smaller and
-            // closer together (more dense) than on mobile platforms.
-            visualDensity: VisualDensity.adaptivePlatformDensity,
+    return MaterialApp(
+      title: 'Vasculink App',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: MyHomePage(title: 'Vasculink'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key key, this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  int _counter = 0;
+
+  void _incrementCounter() {
+    setState(() {
+      _counter++;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+
+    return Scaffold(
+
+      appBar: PreferredSize(
+        preferredSize: Size.fromHeight(130.0),
+          child: AppBar(
+            elevation: 0,
+            flexibleSpace: Padding(
+              padding: const EdgeInsets.fromLTRB(0,65,0,0),
+              child: Row(
+                children:<Widget> [
+                  IconButton(
+                    icon: Icon(Icons.clear),
+                    tooltip: "cancel the entry",
+                    onPressed: (){
+                      // TODO: implement cancel function
+                    },
+                    color: Colors.white,
+                    iconSize: 30,
+                  ),
+                  Text(
+                      'Result',
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 24,
+                      ),
+                  ),
+              ],
+              ),
+            ),
+            backgroundColor: Colors.blue[400],
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.vertical(
+                bottom: Radius.circular(29),
+              ),
+            ),
+          )
+      ),
+
+      backgroundColor: Colors.grey[100],
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children:<Widget> [
+          Text(
+            'Patient Score',
+            style: TextStyle(
+              color: Colors.blue[400],
+              fontSize: 28,
+              fontWeight: FontWeight.bold,
+            ),
           ),
-          home: ReduxDemoPage(),
-        ));
+          SizedBox(
+            height: 10,
+          ),
+          Card(
+            elevation: 0.0,
+            margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 60.0),
+            child: Column(
+              children:<Widget> [
+                SizedBox(
+                  height: 20,
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children:<Widget> [
+                    Text('4',
+                      style: TextStyle(
+                        color: Colors.blue[400],
+                        fontSize: 55,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    Text('/',
+                      style: TextStyle(
+                        color: Colors.blue[400],
+                        fontSize: 25,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    Text('7',
+                      style: TextStyle(
+                        color: Colors.blue[400],
+                        fontSize: 25,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ],
+                ),
+                SizedBox(
+                  height: 10,
+                ),
+                Text('Medium Risk',
+                  style: TextStyle(
+                    color: Colors.black26,
+                    fontSize: 25,
+                ),
+                ),
+                SizedBox(
+                  height: 20,
+                ),
+              ],
+            ),
+          ),
+          Container(
+              child: Image.asset(
+                  'images/dial.png',
+                  height: 150,
+//                  width: 100,
+                ),
+              ),
+        ],
+      ),
+    );
   }
 }
